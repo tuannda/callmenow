8df2c6e2a7999fe16a76c9e0bb13ff04d653439c
TuanNDA <tuannda@outlook.sa>
2023-03-26 00:28:15 +0700

    .

--- views/callMe.php
+++ views/callMe.php
@@ -1,1 +1,9 @@
-sdfs
+Cắt tóc
+Gội đầu
+Uốn – Duỗi – Nhuộm
+Make up
+Massage
+Nail – Wax
+SPA
+Liệu trình điều trị cho da đầu
+Phục hồi chuyên sâu
--- views/layout/main.php
+++ views/layout/main.php
@@ -8,12 +8,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Welcome</title>
-    <meta name="description" content="Lets create something wonderful">
+    <title>Bông Phượng Salon</title>
+    <meta name="description" content="Bông Phương Hair Salon đều được tư vấn bởi đội ngũ nhân viên chuyên nghiệp, giàu kinh nghiệm dưới sự chỉ đạo của Nhà tạo mẫu tóc dày dặn kinh nghiệm">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 </head>
 <body>
 <main id="app" role="main" class="container">
     <?= $content ?>
 </main>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
